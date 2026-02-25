6a124a917994b3d3d38fc90617a46398d14fe418
im-nathan-m <nlmargol@ucsc.edu>
2026-02-25 14:36:26 -0800

    Initial commit

---
+++ model.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms, models
+
+# ------------ Step 1. Data Visualization ------------
+pass
+
+# ------------ Step 2. Data Preprocessing ------------
+pass
