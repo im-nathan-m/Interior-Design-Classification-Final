f1f50e739e8c5957fb98a31d45721fda22dbd1b5
im-nathan-m <nlmargol@ucsc.edu>
2026-02-26 10:18:22 -0800

    Create datasets & dataloaders for train/test

--- model.py
+++ model.py
@@ -7,8 +7,30 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms, models
 
+BATCH_SIZE = 100
+ROOT_DIR = "archive"
+
 # ------------ Step 1. Data Visualization ------------
 pass
 
 # ------------ Step 2. Data Preprocessing ------------
-pass
+train_transforms = transforms.Compose([
+    transforms.Resize((100, 100)),
+    transforms.RandomHorizontalFlip(p=0.2),
+    transforms.ToTensor()
+])
+
+test_transforms = transforms.Compose([
+    transforms.Resize((100, 100)),
+    transforms.ToTensor()
+])
+
+train_dataset = datasets.ImageFolder(root=f'{ROOT_DIR}/dataset_train', transform=train_transforms)
+test_dataset = datasets.ImageFolder(root=f'{ROOT_DIR}/dataset_test', transform=test_transforms)
+
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+print(train_dataset.classes)
+
+NUM_CLASSES = len(train_dataset.classes)
